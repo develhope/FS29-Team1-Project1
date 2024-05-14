cf4c9ebe09ae571cfe3e5f8010b7f569d387e4d4
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-05-14 18:02:18 +0200

    Fix: add object in teams

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const teams = [];
+const teams = [
+  {
+    name: "Giuseppe",
+    surname: "Incurvati",
+    age: 30,
+    city: "Roma",
+    hobby: "Fumetti",
+    favoriteFood: "Purè di patate",
+    favoriteVideogame: "Final Fantasy",
+    favoriteFilm: "Batman",
+    favoriteBook: "Persy Jackson",
+    petName: "Luna, beagle",
+  },
+];
