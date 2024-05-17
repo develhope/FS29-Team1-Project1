b7f4f2d07c8bb37c21791696e7285f4d6125e125
Emanuele <emanuelevutera@yahoo.it>
2024-05-17 11:49:44 +0200

    object ema

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const teams = [];
+const teams = [
+  {
+    name: "Emanuele",
+    surname: "Vutera",
+    age: 27,
+    city: "Perugia",
+    hobby: "Videogames",
+    favoriteFood: "Pasta con il ragù",
+    favoriteVideoGame: "League of Legends",
+    favoriteFilm: "Cambia la tua vita con un click",
+    favoriteBook: "",
+    petName: "",
+  },
+];
