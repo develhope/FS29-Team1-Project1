d622d6935b2066ee50a850ff72eafcb0fce4ce8d
Gioele Brunori <brunorigioele257@gmail.com>
2024-05-17 11:26:31 +0200

    add object gioele

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-const teams = [];
+const teams = [{
+name:'Gioele',
+surname:'Brunori',
+age:24,
+city:'Cingoli',
+hobby:'kick box',
+favoriteFood:'pasta',
+favoriteVideoGame:'dark souls 3',
+favoriteFilm:'roadhouse',
+favoriteBook:'la casa',
+petName:'Randal'
+
+},];
