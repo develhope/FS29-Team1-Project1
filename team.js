4e86946c03109cb6f170fa3e79c4a2883d408163
Francesco Simeone <insanecrusher89@gmail.com>
2024-05-17 11:58:05 +0200

    add object Francesco

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const teams = [];
+const teams = [
+  {
+    name: "Francesco",
+    surname: "Simeone",
+    age: 34,
+    city: "Latiano",
+    hobby: "Football 5-a-side",
+    favoriteFood: "Carbonara",
+    favoriteVideoGame: "Final Fantasy 7",
+    favoriteFilm: "Kimi no na wa - Your-name",
+    favoriteBook: "The Metamorphosis",
+    petName: "Loki",
+  },
+];
