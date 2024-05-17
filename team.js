e4b51c249e994340f5fc5ff6b6a6277f6325ea4a
Luca Potenza <potenzaluca32@gmail.com>
2024-05-17 11:26:25 +0200

    add object luca

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const teams = [];
+const teams = [{
+    name: "Luca",
+surname: "Potenza",
+age: 29,
+city: "Roma",
+hobby: "Gaming",
+favoriteFood: "Pizza",
+favoriteVideoGame: "Red Redemption 2",
+favoriteFilm: "Old Henry",
+favoriteBook: "Tex",
+petName: "Totò & Sammy"
+}];
